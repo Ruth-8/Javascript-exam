85b55e74001d83e1c3b1629153e4e2ee0b351c77
Ruth-8 <rutasisimbajevaite@yahoo.com>
2022-07-12 00:17:13 +0300

    Finished

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,16 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor (title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = +budget;
+    }
+    wasExpensive(){
+    return (this.budget > 100000000 ? "true":"false");
+    }}
+
+const movie1 = new Movie ("Spectre", "Sam Mendes", 300000000);
+console.log(movie1);
+movie1.wasExpensive();
