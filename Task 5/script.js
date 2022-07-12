d6e6a4c79e6767add1f952099d90123ba828b0b8
Ruth-8 <rutasisimbajevaite@yahoo.com>
2022-07-12 20:47:36 +0300

    Finished

--- Task 5/script.js
+++ Task 5/script.js
@@ -2,7 +2,7 @@
 Turimas "users" masyvas. 
 
 Parašykite funckijas, kurios atlikas nurodytas užduotis:
-1. funkcija "filterDogOwers" - kaip argumentą priims masyvą ir duoto masyvo 
+1. funkcija "filterDogOwners" - kaip argumentą priims masyvą ir duoto masyvo 
 atveju grąžins "users", kurie turi augintinį.
 2. funkcija "filterAdults" - kaip argumentą priims masyvą ir duoto masyvo 
 atveju grąžins masyvą su "users", kurie yra pilnamečiai.
@@ -20,11 +20,13 @@ const users = [
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
 
+//1
+const filters = [true];
 
+const filterDogOwners = users.filter(val => filters.includes(val.hasDog));
+console.log(filterDogOwners)
 
-
-
-//2
+2
 const filterAdults = users.filter(function(user){
   return user.age >=18
 })
