09dd68d5e53eba04da7ff5df4f1aa0b9c1e27397
Ruth-8 <rutasisimbajevaite@yahoo.com>
2022-07-12 20:48:31 +0300

    update

--- Task 4/script.js
+++ Task 4/script.js
@@ -19,14 +19,6 @@ fetch(endpoint).then((response) =>{
     if(!response.ok) {
         throw new Error("Error: " + response.status);
     }
-    return response.json();
+    return response.json()
 })
-
-
-
-
-
-
-
-
 
